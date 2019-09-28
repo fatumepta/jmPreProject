ae14184ccb1b10ee5e40c2bae9d3d5671d90cc8d
fatumepta <fatumepta-git@yandex.ru>
2019-09-28 03:25:38 +0300

    FINAL COMMIT on Task 2 (Port to Hibernate)

--- src/main/java/dao/UserDaoHibernate.java
+++ src/main/java/dao/UserDaoHibernate.java
@@ -2,7 +2,7 @@ package dao;
 
 import models.User;
 import org.hibernate.Session;
-import org.hibernate.query.Query;
+import org.hibernate.Transaction;
 
 import java.util.List;
 import java.util.Optional;
@@ -16,40 +16,61 @@ public class UserDaoHibernate implements Dao<User> {
 
     @Override
     public Optional<User> get(long id) {
-        return Optional.empty();
+        Transaction transaction = session.beginTransaction();
+        Optional<User> user = Optional.of(session.get(User.class, id));
+
+        transaction.commit();
+        return user;
+
     }
 
     @Override
     public List<User> getAll() {
-        try {
-            return (List<User>) session.createQuery("from User").list();
-        } finally {
-            session.close();
-        }
+        Transaction transaction = session.beginTransaction();
+        List<User> users = (List<User>) session.createQuery("from User").list();
+        transaction.commit();
+
+        return users;
     }
 
     @Override
     public void add(User user) {
-
+        Transaction transaction = session.beginTransaction();
+        session.save(user);
+        transaction.commit();
     }
 
     @Override
     public void update(User user, String[] params) {
+        Transaction transaction = session.beginTransaction();
+        session.createQuery("update User set name=:name, login=:login, password=:password where id=:id")
+                .setParameter("name", params[0])
+                .setParameter("login", params[1])
+                .setParameter("password", params[2])
+                .setParameter("id", user.getId())
+                .executeUpdate();
 
+        transaction.commit();
     }
 
     @Override
     public void delete(User user) {
+        Transaction transaction = session.beginTransaction();
+        session.createQuery("delete User where id=:id")
+                .setParameter("id", user.getId())
+                .executeUpdate();
 
+        transaction.commit();
     }
 
     @Override
     public void createTable() {
-
+        // hbm2ddl -> create
     }
+
 
     @Override
     public void dropTable() {
-
+        // hbm2ddl -> create (create-drop)
     }
 }
